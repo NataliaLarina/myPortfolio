78bc135293a764538011af237c7fab8f946acaf1
Natalia Larina <koshka_208@yahoo.com>
2016-05-02 14:57:23 -0500

    flip.js for portfolio added

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,25 @@
-$("html").niceScroll({
-      scrollspeed: 51,
-      mousescrollstep: 45,
-     });
+// $("html").niceScroll({
+//       scrollspeed: 51,
+//       mousescrollstep: 45,
+//      });
+//
+//     $(function() {
+//         $('a.page-scroll').bind('click', function(event) {
+//             var $anchor = $(this);
+//             $('html, body').stop().animate({
+//                 scrollTop: $($anchor.attr('href')).offset().top
+//             }, 1000);
+//             event.preventDefault();
+//         });
+//     });
+//
+//
 
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1000);
-            event.preventDefault();
-        });
-    });
+
+$("#card").flip({
+  trigger: 'click',
+  forceHeight: '200px',
+  forceWidth: '200px'
+});
+
+console.log($('#card'))
